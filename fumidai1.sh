ec2cc48e2a466de4d7a5b1ea9039c2aa769e966f
echoliu <547341734@qq.com>
2017-06-06 09:56:25 +0800

    Create fumidai1.sh

---
+++ fumidai1.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+servers=(
+    "10.138.0.3"
+    "10.138.0.4"
+    "10.130.170.10"
+    )
+
+PS3="连接服务器?"
+
+select server in "${servers[@]}"; do
+    echo "连接$server"
+    ssh "$server"
+done
